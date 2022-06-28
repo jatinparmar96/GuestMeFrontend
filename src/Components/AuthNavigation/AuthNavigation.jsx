7674fb68811ad31bde2d13221d9aa0931ed1fc95
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-06-28 00:13:32 -0700

    Add Update Profile link for demo

--- src/Components/AuthNavigation/AuthNavigation.jsx
+++ src/Components/AuthNavigation/AuthNavigation.jsx
@@ -13,6 +13,11 @@ export const AuthNavigation = () => {
       <Link to="/find-a-speaker" className={styles.link}>
         Find a Speaker
       </Link>
+
+      {/* TODO: Remove this link  */}
+      <Link to="/speakers/update/profile" className={styles.link}>
+        Update Profile
+      </Link>
     </div>
   );
 };
