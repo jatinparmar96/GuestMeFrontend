4492ec95b9d38e22f52a1a87f72357d329171bf0
Koichi <koichi89592@gmail.com>
2022-06-29 19:55:07 -0700

    fix: button related file names

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,7 +1,12 @@
-import { RequestButtonContainer, RequestButtonContainerWide, SaveButtonContainer, SubmitButtonContainer } from './SubmitButton.styles';
+import {
+  RequestButtonContainer,
+  RequestButtonContainerWide,
+  SaveButtonContainer,
+  SubmitButtonContainer,
+} from './Buttons.styles';
 
 /**
- * @type {React.FC<import('./SubmitButtonTypes').SubmitButtonProps>}
+ * @type {React.FC<import('./ButtonsTypes.d').SubmitButtonProps>}
  */
 export const SubmitButton = (props) => (
   <SubmitButtonContainer onClick={props.onClick}>
@@ -9,19 +14,19 @@ export const SubmitButton = (props) => (
   </SubmitButtonContainer>
 );
 
-export const SaveButton= (props) => (
+export const SaveButton = (props) => (
   <SaveButtonContainer onClick={props.onClick}>
     {props.text}
   </SaveButtonContainer>
 );
 
-export const RequestButton= (props) => (
+export const RequestButton = (props) => (
   <RequestButtonContainer onClick={props.onClick}>
     {props.text}
   </RequestButtonContainer>
 );
 
-export const RequestButtonWide= (props) => (
+export const RequestButtonWide = (props) => (
   <RequestButtonContainerWide onClick={props.onClick}>
     {props.text}
   </RequestButtonContainerWide>
--- src/Containers/Speaker_PrimaryInformation/SpeakerPrimaryInformation.jsx
+++ src/Containers/Speaker_PrimaryInformation/SpeakerPrimaryInformation.jsx
@@ -1,26 +1,24 @@
+import { RequestButton, SaveButton } from '../../Components/Buttons/Buttons';
 import SpeakerInfo from '../../Components/SpeakerProfileInfo/SpeakerProfileInfo';
-import { RequestButton, SaveButton } from '../../Components/SubmitButton/SubmitButton';
 
-const SpeakerPrimaryInformation = props => {
-
-
+const SpeakerPrimaryInformation = (props) => {
   return (
     <>
       <div>
         <SpeakerInfo speaker={props.speaker} />
-              <div className="SaveButtonContainer">
-        <SaveButton
-          text="Save"
-          // onClick={}
-        />
-         <RequestButton
-          text="Request"
-          // onClick={}
-        />
-      </div>
+        <div className="SaveButtonContainer">
+          <SaveButton
+            text="Save"
+            // onClick={}
+          />
+          <RequestButton
+            text="Request"
+            // onClick={}
+          />
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default SpeakerPrimaryInformation;
