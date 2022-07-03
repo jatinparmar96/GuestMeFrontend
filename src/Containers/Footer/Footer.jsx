32c07d1aa527b81243d65bbc9f6e376098c5d0e9
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-03 04:19:07 -0700

    Style: footer mobile

--- src/Containers/Footer/Footer.jsx
+++ src/Containers/Footer/Footer.jsx
@@ -1,5 +1,23 @@
+import instagram from '../../assets/instagram.png';
+import linkedin from '../../assets/linkedin.png';
+import logo_mic from '../../assets/logo-mic-green.png';
+import logo from '../../assets/logo-white.png';
+import twitter from '../../assets/twitter.png';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return <footer className={styles.footer}>
+    <img src={logo_mic} alt="logo" className={styles.logoMic}></img>
+    <img src={logo} alt="logo" className={styles.logo}></img>
+    <div className={styles.socialIcons}>
+      <img src={twitter} alt="twitter logo" className={styles.social}></img>
+      <img src={linkedin} alt="linkedin logo" className={styles.social}></img>
+      <img src={instagram} alt="instagram logo" className={styles.social}></img>
+
+    </div>
+
+    <p className={styles.copy}>© 2022 GUEST ME All rights reserved.</p>
+
+
+  </footer>;
 };
