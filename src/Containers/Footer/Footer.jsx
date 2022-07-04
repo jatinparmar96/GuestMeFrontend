9f7f70b298d38c937fdd932ddc12e800ecb795d0
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-03 17:49:47 -0700

    Style: footer desktop

--- src/Containers/Footer/Footer.jsx
+++ src/Containers/Footer/Footer.jsx
@@ -1,21 +1,16 @@
-import instagram from '../../assets/instagram.png';
-import linkedin from '../../assets/linkedin.png';
 import logo_mic from '../../assets/logo-mic-green.png';
 import logo from '../../assets/logo-white.png';
-import twitter from '../../assets/twitter.png';
+import DesktopNavigation from '../../Components/navigation/desktop-navigation/DesktopNavigation';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return <footer className={styles.footer}>
     <img src={logo_mic} alt="logo" className={styles.logoMic}></img>
     <img src={logo} alt="logo" className={styles.logo}></img>
-    <div className={styles.socialIcons}>
-      <img src={twitter} alt="twitter logo" className={styles.social}></img>
-      <img src={linkedin} alt="linkedin logo" className={styles.social}></img>
-      <img src={instagram} alt="instagram logo" className={styles.social}></img>
 
-    </div>
 
+    <p className={styles.description}>GUEST ME for your schools, for your students to give them opportunities to learn new things.</p>
+    <DesktopNavigation className={styles.navigationFooter} footer={true} />
     <p className={styles.copy}>© 2022 GUEST ME All rights reserved.</p>
 
 
