4b835b880d607cd4d9aaf9536d5c3253f3699013
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-07-04 23:53:39 -0700

    Fix: merge and build errors

--- src/Pages/SpeakerBooking/SpeakerBooking.jsx
+++ src/Pages/SpeakerBooking/SpeakerBooking.jsx
@@ -4,33 +4,24 @@ import { getSpeakerBookings } from '../../Api/Speaker.service';
 import SpeakerBookingList from '../../Components/SpeakerBookingList/SpeakerBookingList';
 import tokenAtom from '../../Recoil/Authentication/atom';
 
-
 const SpeakerBooking = (props) => {
-
   const [loadingState, setLoadingState] = useState(true);
-  const [ speakerData, setSpeakerData ] = useState();
-  const [user, setUser] = useRecoilState(tokenAtom);
-
-
-
-
+  const [speakerData, setSpeakerData] = useState();
+  const [user] = useRecoilState(tokenAtom);
 
   useEffect(() => {
-
     setLoadingState(true);
-    if(user.name._id){
-    getSpeakerBookings(user.name._id)
-      .then((response) => {
-        // console.log(response)
-        if (response) {
-
-          setSpeakerData(response.data);
-          setLoadingState(false);
-
-        }
-      })
-      .catch((error) => console.log(error));
-  }
+    if (user.name._id) {
+      getSpeakerBookings(user.name._id)
+        .then((response) => {
+          // console.log(response)
+          if (response) {
+            setSpeakerData(response.data);
+            setLoadingState(false);
+          }
+        })
+        .catch((error) => console.log(error));
+    }
   }, [user]);
 
   return (
@@ -39,8 +30,7 @@ const SpeakerBooking = (props) => {
         <p>Loading... </p>
       ) : (
         <>
-          <SpeakerBookingList speaker ={speakerData} />
-
+          <SpeakerBookingList speaker={speakerData} />
         </>
       )}
     </>
