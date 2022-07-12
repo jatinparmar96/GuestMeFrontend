897a223ee19c9c7fbca495846fa670beb908ff8f
Koichi <koichi89592@gmail.com>
2022-07-11 23:52:04 -0700

    fix heading style

--- src/Components/PageHeading/PageHeading.jsx
+++ src/Components/PageHeading/PageHeading.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import style from './PageHeading.module.scss'
+import style from './PageHeading.module.scss';
 
 /**@type {React.FC<{text: string}>} */
 export const PageHeading = (props) => {
   return (
     <div className={style.pageHeading}>
       <h2 className={style.heading}>{props.text}</h2>
-      <div className="right">
-        <p>
-          <span>Icon</span>Firstname Lastname
-        </p>
-        <button>Logout</button>
-      </div>
-      {/* Bread Crumbs may be put */}
     </div>
   );
 };
