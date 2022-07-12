cd22775d5305ee27e6ac87c2ea2e274aaa69ea3d
Koichi <koichi89592@gmail.com>
2022-07-11 23:51:56 -0700

    fix bread crumbs style

--- src/Components/breadCrumbs/BreadCrumbs.jsx
+++ src/Components/breadCrumbs/BreadCrumbs.jsx
@@ -1,11 +1,12 @@
 //@ts-check
 import React from 'react';
+import style from './BreadCrumbs.module.scss';
 
 /**@type {React.FC<{currentPosition: string}>} */
 const BreadCrumbs = (props) => {
   return (
-    <div>
-      <div>
+    <div className={style.breadCrumbsContainer}>
+      <div className={style.breadCrumbs}>
         Home {'>'} {props.currentPosition}
       </div>
     </div>
