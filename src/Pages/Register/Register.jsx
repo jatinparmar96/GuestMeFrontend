1062c8e8773ab0ae86dd434e2e102696b16249f5
Koichi <koichi89592@gmail.com>
2022-07-12 17:42:36 -0700

    modify: registration page component

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,15 @@
 // @ts-check
-import { useState } from 'react';
+import React, { useState } from 'react';
 
+import RegistrationImage from '../../assets/registration_speaker.png';
+import BreadCrumbs from '../../Components/breadCrumbs/BreadCrumbs';
+import { PageHeading } from '../../Components/PageHeading/PageHeading';
 import { OrganizationRegisterContainer } from '../../Containers/Registers/OrganizationRegisterContainer/OrganizationRegisterContainer';
 import { SpeakerRegisterContainer } from '../../Containers/Registers/SpeakerRegisterContainer/SpeakerRegisterContainer';
 
-import styles from './Register.module.scss';
+import style from './Register.module.scss';
 
+/**@type {React.FC<any>} */
 export const Register = (props) => {
   const [isSpeaker, setIsSpeaker] = useState(true);
 
@@ -17,26 +21,35 @@ export const Register = (props) => {
 
   return (
     <>
-      <div className={styles.radioContainer}>
-        <input
-          type="radio"
-          name="role"
-          id="role-speaker"
-          defaultChecked={isSpeaker}
-          onClick={() => setIsSpeaker(true)}
-        />
-        <label htmlFor="role-speaker">I am a speaker</label>
+      <PageHeading text="Register" />
+      <BreadCrumbs currentPosition="Register" />
+      <div className={style.contentContainer}>
+        <div className={style.radioContainer}>
+          <div className={isSpeaker ? style.radioSelected : ''}>
+            <input
+              type="radio"
+              name="role"
+              id="role-speaker"
+              defaultChecked={isSpeaker}
+              onClick={() => setIsSpeaker(true)}
+            />
+            <label htmlFor="role-speaker">I am a speaker</label>
+          </div>
+          <div className={isSpeaker ? '' : style.radioSelected}>
+            <input
+              type="radio"
+              name="role"
+              id="role-organization"
+              onClick={() => setIsSpeaker(false)}
+            />
+            <label htmlFor="role-organization">I am a organization</label>
+          </div>
+        </div>
+        {registerContainer}
+        <div className={style.imageContainer}>
+          <img src={RegistrationImage} alt="speaker" className={style.image} />
+        </div>
       </div>
-      <div>
-        <input
-          type="radio"
-          name="role"
-          id="role-organization"
-          onClick={() => setIsSpeaker(false)}
-        />
-        <label htmlFor="role-organization">I am a organization</label>
-      </div>
-      {registerContainer}
     </>
   );
 };
