34ef2abfc2df797efac1b3f414c9b23f94073dc9
Koichi <koichi89592@gmail.com>
2022-07-14 11:43:17 -0700

    change HOME in bread crumbs to the link

--- src/Components/breadCrumbs/BreadCrumbs.jsx
+++ src/Components/breadCrumbs/BreadCrumbs.jsx
@@ -2,12 +2,18 @@
 import React from 'react';
 import style from './BreadCrumbs.module.scss';
 
+import { Link } from 'react-router-dom';
+
 /**@type {React.FC<{currentPosition: string}>} */
 const BreadCrumbs = (props) => {
   return (
     <div className={style.breadCrumbsContainer}>
       <div className={style.breadCrumbs}>
-        Home {'>'} {props.currentPosition}
+        <Link to="/" className={style.link}>
+          Home
+        </Link>
+        <span>{'>'}</span>
+        <span> {props.currentPosition}</span>
       </div>
     </div>
   );
