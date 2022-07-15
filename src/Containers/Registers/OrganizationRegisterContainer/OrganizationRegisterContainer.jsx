623c69dd081b3403179beaf7d6992523bc59285a
Koichi <koichi89592@gmail.com>
2022-07-15 14:24:34 -0700

    fix: issueRegister page - Placeholder text #92

--- src/Containers/Registers/OrganizationRegisterContainer/OrganizationRegisterContainer.jsx
+++ src/Containers/Registers/OrganizationRegisterContainer/OrganizationRegisterContainer.jsx
@@ -52,86 +52,89 @@ export const OrganizationRegisterContainer = () => {
   return (
     <>
       <div className={style.registerContainer}>
-        <div>
-          <h3 className={style.contentHeading}>
-            Registration Form for the Organization
-          </h3>
-          <p className={style.description}>
-            Please fill out the form below, and click register.
-          </p>
-          <div className={style.form}>
-            <div className={style.nameFiled}>
-              <div className={style.inputField}>
-              <label className={style.label}>Organization Name*</label>
-                <input
-                  type="text"
-                  className={style.input}
-                  autoComplete="organization"
-                  {...register('organizationName', { required: true })}
-                />
-              </div>
-              <div className={style.inputField}>
-              <label className={style.label}>Phone*</label>
-                <input
-                  type="phone"
-                  className={style.input}
-                  {...register('phone', { required: true })}
-                />
-              </div>
-            </div>
+        <h3 className={style.contentHeading}>
+          Registration Form for the Organization
+        </h3>
+        <p className={style.description}>
+          Please fill out the form below, and click register.
+        </p>
+        <div className={style.form}>
+          <div className={style.nameFiled}>
             <div className={style.inputField}>
-            <label className={style.label}>Email*</label>
+              <label className={style.label}>Organization Name*</label>
               <input
-                type="email"
+                type="text"
                 className={style.input}
-                autoComplete="email"
-                {...register('email', { required: true })}
+                autoComplete="organization"
+                placeholder="Name"
+                {...register('organizationName', { required: true })}
               />
             </div>
+            <div className={style.inputField}>
+              <label className={style.label}>Phone*</label>
+              <input
+                type="phone"
+                className={style.input}
+                placeholder="012-345-6789"
+                {...register('phone', { required: true })}
+              />
+            </div>
+          </div>
+          <div className={style.inputField}>
+            <label className={style.label}>Email*</label>
+            <input
+              type="email"
+              className={style.input}
+              autoComplete="email"
+              placeholder="organization@guestme.org"
+              {...register('email', { required: true })}
+            />
+          </div>
 
-            {/* {errors.email?.type === "required" && "Password is required"} */}
-            <div className={style.inputField}>
+          {/* {errors.email?.type === "required" && "Password is required"} */}
+          <div className={style.inputField}>
             <label className={style.label}>Password*</label>
-              <div className={style.passwordContainer}>
-                <input
-                  type={passwordVisible ? 'text' : 'password'}
-                  className={`${style.input} ${style.passwordField}`}
-                  {...register('password', { required: true })}
+            <div className={style.passwordContainer}>
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                className={`${style.input} ${style.passwordField}`}
+                placeholder="････････"
+                {...register('password', { required: true })}
+              />
+              <div className={style.eyesContainer}>
+                <Eyes
+                  isVisible={passwordVisible}
+                  handleClick={handleClickPasswordHidden}
                 />
-                <div className={style.eyesContainer}>
-                  <Eyes
-                    isVisible={passwordVisible}
-                    handleClick={handleClickPasswordHidden}
-                  />
-                </div>
               </div>
             </div>
-            {/* {errors.password?.type === "required" && "Password is required"} */}
-            <div className={style.inputField}>
+          </div>
+          {/* {errors.password?.type === "required" && "Password is required"} */}
+          <div className={style.inputField}>
             <label className={style.label}>Confirm Password*</label>
-              <div className={style.passwordContainer}>
-                <input
-                  type={confirmPasswordVisible ? 'text' : 'password'}
-                  className={`${style.input} ${style.passwordField}`}
-                  {...register('confirmPassword', { required: true })}
+            <div className={style.passwordContainer}>
+              <input
+                type={confirmPasswordVisible ? 'text' : 'password'}
+                className={`${style.input} ${style.passwordField}`}
+                placeholder="････････"
+                {...register('confirmPassword', { required: true })}
+              />
+              <div className={style.eyesContainer}>
+                <Eyes
+                  isVisible={confirmPasswordVisible}
+                  handleClick={handleClickConfirmPasswordHidden}
                 />
-                <div className={style.eyesContainer}>
-                  <Eyes
-                    isVisible={confirmPasswordVisible}
-                    handleClick={handleClickConfirmPasswordHidden}
-                  />
-                </div>
               </div>
             </div>
-            <div className={style.submitButtonContainer}>
-              <SubmitButton text="Register" onClick={handleSubmit(onSubmit)} />
-            </div>
-            <div className={style.confirmAccount}>
-              <p>Already have an account?</p>
-              <Link to="/login" className={style.registerLink}>
-                Log in
-              </Link>
-            </div>
+          </div>
+          <div className={style.submitButtonContainer}>
+            <SubmitButton text="Register" onClick={handleSubmit(onSubmit)} />
+          </div>
+          <div className={style.confirmAccount}>
+            <p>Already have an account?</p>
+            <Link to="/login" className={style.registerLink}>
+              Log in
+            </Link>
           </div>
         </div>
       </div>
--- src/Containers/Registers/SpeakerRegisterContainer/SpeakerRegisterContainer.jsx
+++ src/Containers/Registers/SpeakerRegisterContainer/SpeakerRegisterContainer.jsx
@@ -56,87 +56,90 @@ export const SpeakerRegisterContainer = (props) => {
   return (
     <>
       <div className={style.registerContainer}>
-        <div>
-          <h3 className={style.contentHeading}>
-            Registration Form for the Speaker
-          </h3>
-          <p className={style.description}>
-            Please fill out the form below, and click register.
-          </p>
-          <div className={style.form}>
-            <div className={style.nameFiled}>
-              <div className={style.inputField}>
-              <label className={style.label}>First Name*</label>
-                <input
-                  type="text"
-                  className={style.input}
-                  autoComplete="given-name"
-                  {...register('userName', { required: true })}
-                />
-              </div>
-              <div className={style.inputField}>
-              <label className={style.label}>Last Name*</label>
-                <input
-                  type="text"
-                  className={style.input}
-                  autoComplete="family-name"
-                  {...register('userLastname', { required: true })}
-                />
-              </div>
-            </div>
+        <h3 className={style.contentHeading}>
+          Registration Form for the Speaker
+        </h3>
+        <p className={style.description}>
+          Please fill out the form below, and click register.
+        </p>
+        <div className={style.form}>
+          <div className={style.nameFiled}>
             <div className={style.inputField}>
-            <label className={style.label}>Email*</label>
+              <label className={style.label}>First Name*</label>
               <input
-                type="email"
+                type="text"
                 className={style.input}
-                autoComplete="email"
-                {...register('email', { required: true })}
+                placeholder="First"
+                autoComplete="given-name"
+                {...register('userName', { required: true })}
               />
             </div>
+            <div className={style.inputField}>
+              <label className={style.label}>Last Name*</label>
+              <input
+                type="text"
+                className={style.input}
+                placeholder="Last"
+                autoComplete="family-name"
+                {...register('userLastname', { required: true })}
+              />
+            </div>
+          </div>
+          <div className={style.inputField}>
+            <label className={style.label}>Email*</label>
+            <input
+              type="email"
+              className={style.input}
+              autoComplete="email"
+              placeholder="speaker@gmail.com"
+              {...register('email', { required: true })}
+            />
+          </div>
 
-            {/* {errors.email?.type === "required" && "Password is required"} */}
-            <div className={style.inputField}>
+          {/* {errors.email?.type === "required" && "Password is required"} */}
+          <div className={style.inputField}>
             <label className={style.label}>Password*</label>
-              <div className={style.passwordContainer}>
-                <input
-                  type={passwordVisible ? 'text' : 'password'}
-                  className={`${style.input} ${style.passwordField}`}
-                  {...register('password', { required: true })}
+            <div className={style.passwordContainer}>
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                className={`${style.input} ${style.passwordField}`}
+                placeholder="････････"
+                {...register('password', { required: true })}
+              />
+              <div className={style.eyesContainer}>
+                <Eyes
+                  isVisible={passwordVisible}
+                  handleClick={handleClickPasswordHidden}
                 />
-                <div className={style.eyesContainer}>
-                  <Eyes
-                    isVisible={passwordVisible}
-                    handleClick={handleClickPasswordHidden}
-                  />
-                </div>
               </div>
             </div>
-            {/* {errors.password?.type === "required" && "Password is required"} */}
-            <div className={style.inputField}>
+          </div>
+          {/* {errors.password?.type === "required" && "Password is required"} */}
+          <div className={style.inputField}>
             <label className={style.label}>Confirm Password*</label>
-              <div className={style.passwordContainer}>
-                <input
-                  type={confirmPasswordVisible ? 'text' : 'password'}
-                  className={`${style.input} ${style.passwordField}`}
-                  {...register('confirmPassword', { required: true })}
+            <div className={style.passwordContainer}>
+              <input
+                type={confirmPasswordVisible ? 'text' : 'password'}
+                className={`${style.input} ${style.passwordField}`}
+                placeholder="････････"
+                {...register('confirmPassword', { required: true })}
+              />
+              <div className={style.eyesContainer}>
+                <Eyes
+                  isVisible={confirmPasswordVisible}
+                  handleClick={handleClickConfirmPasswordHidden}
                 />
-                <div className={style.eyesContainer}>
-                  <Eyes
-                    isVisible={confirmPasswordVisible}
-                    handleClick={handleClickConfirmPasswordHidden}
-                  />
-                </div>
               </div>
             </div>
-            <div className={style.submitButtonContainer}>
-              <SubmitButton text="Register" onClick={handleSubmit(onSubmit)} />
-            </div>
-            <div className={style.confirmAccount}>
-              <p>Already have an account?</p>
-              <Link to="/login" className={style.registerLink}>
-                Log in
-              </Link>
-            </div>
+          </div>
+          <div className={style.submitButtonContainer}>
+            <SubmitButton text="Register" onClick={handleSubmit(onSubmit)} />
+          </div>
+          <div className={style.confirmAccount}>
+            <p>Already have an account?</p>
+            <Link to="/login" className={style.registerLink}>
+              Log in
+            </Link>
           </div>
         </div>
       </div>
