ce55f31a91cf8ff05e83641a2bd2f01f36c6e832
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-17 22:27:24 -0700

    Add: certifications style mobile

--- src/Components/SpeakersCertifications/SpeakerCertifications.jsx
+++ src/Components/SpeakersCertifications/SpeakerCertifications.jsx
@@ -1,9 +1,10 @@
+import style from './SpeakerCertifications.module.scss';
 const SpeakerCertifications = (props) => {
 
     return (
       <>
         <div>
-          <section className='profileCertifications'>
+          <section className={style.profileCertifications}>
             <h4>Certifications</h4>
             <p>{props.speaker.certifications.join(", ")}</p>
           </section>
