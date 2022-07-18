53e19d20f03c94e00ae58c46a074298fd2732e38
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-17 22:06:44 -0700

    Add: about me styling

--- src/Containers/Speaker_SecondaryContainer/Speaker_SecondaryContainer.jsx
+++ src/Containers/Speaker_SecondaryContainer/Speaker_SecondaryContainer.jsx
@@ -4,16 +4,26 @@ import SpeakerAboutMe from '../../Components/SpeakerAboutMe/SpeakerAboutMe';
 import SpeakerCertifications from '../../Components/SpeakersCertifications/SpeakerCertifications';
 import SpeakerSkills from '../../Components/SpeakerSkills/SpeakerSkills';
 import SpeakerReviews from '../../Components/SpeakersReviews/SpeakerReviews';
+import style from './SpeakerSecondaryContainer.module.scss';
 
 const SpeakerSecondaryInformation = (props) => {
   const navigate = useNavigate();
   return (
     <>
-      <div>
-        <SpeakerAboutMe speaker={props.speaker} />
-        <SpeakerSkills speaker={props.speaker} />
-        <SpeakerCertifications speaker={props.speaker} />
-        <SpeakerReviews speaker={props.speaker} />
+      <div className={style.secondary}>
+        <div className={style.about}>
+          <h4>About me</h4>
+          <SpeakerAboutMe speaker={props.speaker} />
+        </div>
+        <div className={style.skills}>
+          <SpeakerSkills speaker={props.speaker} />
+        </div>
+        <div className={style.certifications}>
+          <SpeakerCertifications speaker={props.speaker} />
+        </div>
+        <div className={style.reviews}>
+          <SpeakerReviews speaker={props.speaker} />
+        </div>
         <div className="SaveButtonContainer">
           <SaveButton
             text="Save"
