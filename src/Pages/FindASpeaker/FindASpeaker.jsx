60361156ed72d4c76cbebb92bd8b95703939ed63
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-07-18 22:56:24 -0700

    Chore: remove unused variables

--- src/Pages/FindASpeaker/FindASpeaker.jsx
+++ src/Pages/FindASpeaker/FindASpeaker.jsx
@@ -10,26 +10,11 @@ import style from './FindASpeaker.module.scss';
 
 /**@type {React.FC<any>} */
 export const FindASpeaker = (props) => {
-  /**@type {[string[], React.Dispatch<string[]>]} */
-  const [areas, setAreas] = useState([]);
-  /**@type {[string[], React.Dispatch<string[]>]} */
-  const [deliveryMethod, setDeliveryMethod] = useState([]);
-  /**@type {[string[], React.Dispatch<string[]>]} */
-  const [languages, setLanguages] = useState([]);
-  /**@type {[string[], React.Dispatch<string[]>]} */
-  const [locations, setLocations] = useState([]);
-  /**@type {[number[], React.Dispatch<number[]>]} */
-  const [price, setPrice] = useState([0, Infinity]);
-
   /**@type {[number, React.Dispatch<number>]} */
   const [count, setCount] = useState();
 
   /**@type {[SpeakerResponse[], React.Dispatch<SpeakerResponse[]>]} */
   const [speakers, setSpeakers] = useState([]);
-
-  /**
-   * @description Handles the change of all states
-   */
 
   const filter = useRecoilValue(filterParamsSelector);
 
@@ -43,49 +28,13 @@ export const FindASpeaker = (props) => {
     })();
   }, [filter]);
 
-  const changeAreas = ({ target: { value, checked } }) => {
-    if (checked) {
-      setAreas([...areas, value]);
-    } else {
-      setAreas(areas.filter((area) => area !== value));
-    }
-  };
-
-  const changeDeliveryMethod = ({ target: { value, checked } }) => {
-    if (checked) {
-      setDeliveryMethod([...deliveryMethod, value]);
-    } else {
-      setDeliveryMethod(deliveryMethod.filter((method) => method !== value));
-    }
-  };
-  const changeLanguages = ({ target: { value, checked } }) => {
-    if (checked) {
-      setLanguages([...languages, value]);
-    } else {
-      setLanguages(languages.filter((language) => language !== value));
-    }
-  };
-  const changeLocations = ({ target: { value, checked } }) => {
-    if (checked) {
-      setLocations([...locations, value]);
-    } else {
-      setLocations(locations.filter((location) => location !== value));
-    }
-  };
-
   return (
     <div>
       <PageHeading text="Find a speaker" />
       <div className={style.breadCrumbs}>Home {'>'} Find a speaker</div>
       <div className={style.contentContainer}>
         <aside>
-          <Filter
-            changeAreas={changeAreas}
-            setPrice={setPrice}
-            changeDeliveryMethod={changeDeliveryMethod}
-            changeLanguages={changeLanguages}
-            changeLocations={changeLocations}
-          />
+          <Filter />
         </aside>
         <main>
           <Speakers speakers={speakers} count={count} />
