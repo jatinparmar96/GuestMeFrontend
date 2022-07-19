040aada40932ca8f073853affe3d0bdee18a31e9
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-07-18 22:50:19 -0700

    Feat: Add styling for filter

--- src/Components/landing-page/landing-page-filter/LandingPageFilter.jsx
+++ src/Components/landing-page/landing-page-filter/LandingPageFilter.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import AreaFilterComponent from '../../filter/areaFilter/AreaFilterComponent';
 
+import { ReactComponent as CaretDown } from '../../../assets/icons/caret-down.svg';
 import DeliveryMethodComponent from '../../filter/deliveryMethod/DeliveryMethodComponent';
 import LanguageFilterComponent from '../../filter/languageFilter/LanguageFilterComponent';
 import LocationFilterComponent from '../../filter/locationFilter/LocationFilterComponent';
 import PriceFilterComponent from '../../filter/priceFilter/PriceFilterComponent';
 import style from '././LandingPageFilter.module.scss';
+
 const LandingPageFilter = (props) => {
   const navigate = useNavigate();
   const handleSubmit = (e) => {
@@ -16,7 +18,9 @@ const LandingPageFilter = (props) => {
     <div className={style.filterFormContainer}>
       <form className={style.filterForm}>
         <div className={style.filterContainer}>
-          <label className={style.filterLabel}>Area of expertise</label>
+          <label className={style.filterLabel}>
+            Area of expertise <CaretDown />
+          </label>
           <div className={style.filterValues}>
             <AreaFilterComponent />
           </div>
@@ -36,21 +40,34 @@ const LandingPageFilter = (props) => {
         </div>
 
         <div className={style.filterContainer}>
-          <label className={style.filterLabel}>Language</label>
+          <label className={style.filterLabel}>
+            Language <CaretDown />
+          </label>
           <div className={style.filterValues}>
             <LanguageFilterComponent />
           </div>
         </div>
 
         <div className={style.filterContainer}>
-          <label className={style.filterLabel}>Location</label>
+          <label className={style.filterLabel}>
+            Location <CaretDown />
+          </label>
           <div className={style.filterValues}>
             <LocationFilterComponent />
           </div>
         </div>
 
-        <button onClick={handleSubmit}>Find a speaker</button>
+        <button className={style.primaryButton} onClick={handleSubmit}>
+          Find a speaker
+        </button>
       </form>
+
+      <button
+        className={`${style.primaryButton} ${style.mobileButton}`}
+        onClick={handleSubmit}
+      >
+        Find a speaker
+      </button>
     </div>
   );
 };
