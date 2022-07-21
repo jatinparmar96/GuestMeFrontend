54c873b6042fc392ed694133b76451031286f14e
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-20 21:12:03 -0700

    Add: style menu desktop

--- src/Containers/MyPageMenu/MyPageMenu.jsx
+++ src/Containers/MyPageMenu/MyPageMenu.jsx
@@ -1,12 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { RequestButtonWide } from '../../Components/Buttons/Buttons';
+import ScreenWidth from '../../Components/ScreenSize/ScreenSize';
 import style from './MyPageMenu.module.scss';
 import { SpeakerMenu } from './SpeakerMenu';
-const MyPageMenu = (props) => {
-  let activeStyle = {
-    borderBottom: '3px solid #35AFAC',
-    marginBottom: '-5px'
 
-  };
+
+const MyPageMenu = (props) => {
+  const screen = ScreenWidth();
+  const navigate = useNavigate();
+  const id = JSON.parse(localStorage.getItem('user')).id;
+
+  let activeStyle;
+  if (screen > 992) {
+    activeStyle = {
+      borderRight: '8px solid #35AFAC',
+    };
+  } else {
+      activeStyle = {
+        borderBottom: '3px solid #35AFAC',
+        marginBottom: '-5px'
+      };
+
+  }
+
 
 
 
@@ -18,16 +34,19 @@ const MyPageMenu = (props) => {
       <div className={style.myPageMenuContainer}>
         {
             SpeakerMenu.map((link, index) => {
-            return(
+              return (
+
 
               <NavLink
                 to={link.path}
-                style={({isActive}) => isActive ? activeStyle : undefined}
+                style={({ isActive }) => isActive ? activeStyle : undefined}
+                className={style.myPageMenuLink}
 
-                >
+              >
+
                   <span>{link.icon}</span>
                   <span>{link.title}</span>
-                </NavLink>
+              </NavLink>
 
           )
 
@@ -35,6 +54,14 @@ const MyPageMenu = (props) => {
         }
 
         </div>
+        {screen > 992 ? (
+          <div className={style.button}>
+              <RequestButtonWide
+                text="Preview your profile"
+                onClick={() => navigate('/speakers/' + id)}
+            />
+          </div>
+          ) : null}
         </section>
     </>
 
--- src/Pages/MyPage/MyPage.jsx
+++ src/Pages/MyPage/MyPage.jsx
@@ -1,14 +1,10 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { RequestButtonWide } from '../../Components/Buttons/Buttons';
-import ScreenWidth from '../../Components/ScreenSize/ScreenSize';
+import { Outlet } from 'react-router-dom';
 import MyPageMenu from '../../Containers/MyPageMenu/MyPageMenu';
 import { Helmet } from 'react-helmet';
 import style from './MyPage.module.scss';
 
 const MyPage = () => {
-  const id = JSON.parse(localStorage.getItem('user')).id;
-  const navigate = useNavigate();
-  const screen = ScreenWidth();
+
 
   return (
     <>
@@ -18,12 +14,6 @@ const MyPage = () => {
       <section className={style.myPage}>
         <div>
           <MyPageMenu />
-          {screen > 992 ? (
-            <RequestButtonWide
-              text="Preview your profile"
-              onClick={() => navigate('/speakers/' + id)}
-            />
-          ) : null}
         </div>
         <div className={style.outlet}>
           <Outlet />
