6bfb892b5847bd25228a414e5a4719962b2b4b7c
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-07-21 14:51:01 -0700

    Fix: Key error

--- src/Containers/MyPageMenu/MyPageMenu.jsx
+++ src/Containers/MyPageMenu/MyPageMenu.jsx
@@ -4,41 +4,30 @@ import { SpeakerMenu } from './SpeakerMenu';
 const MyPageMenu = (props) => {
   let activeStyle = {
     borderBottom: '3px solid #35AFAC',
-    marginBottom: '-5px'
-
+    marginBottom: '-5px',
   };
-
-
-
 
   return (
     <>
       <section className={style.myPageMenu}>
-      <h2>Menu</h2>
-      <div className={style.myPageMenuContainer}>
-        {
-            SpeakerMenu.map((link, index) => {
-            return(
-
+        <h2>Menu</h2>
+        <div className={style.myPageMenuContainer}>
+          {SpeakerMenu.map((link, index) => {
+            return (
               <NavLink
+                key={index}
                 to={link.path}
-                style={({isActive}) => isActive ? activeStyle : undefined}
-
-                >
-                  <span>{link.icon}</span>
-                  <span>{link.title}</span>
-                </NavLink>
-
-          )
-
-            })
-        }
-
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                <span>{link.icon}</span>
+                <span>{link.title}</span>
+              </NavLink>
+            );
+          })}
         </div>
-        </section>
+      </section>
     </>
-
   );
-}
+};
 
 export default MyPageMenu;
