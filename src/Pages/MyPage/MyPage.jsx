099b387e6326bcf37fb198a33f5f8d01f3762562
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-07-20 21:12:03 -0700

    Add: style menu desktop

--- src/Pages/MyPage/MyPage.jsx
+++ src/Pages/MyPage/MyPage.jsx
@@ -1,25 +1,15 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { RequestButtonWide } from '../../Components/Buttons/Buttons';
-import ScreenWidth from '../../Components/ScreenSize/ScreenSize';
+import { Outlet } from 'react-router-dom';
 import MyPageMenu from '../../Containers/MyPageMenu/MyPageMenu';
 import style from './MyPage.module.scss';
 
 const MyPage = () => {
-  const id = JSON.parse(localStorage.getItem('user')).id;
-  const navigate = useNavigate();
-  const screen = ScreenWidth();
+
 
   return (
     <>
       <section className={style.myPage}>
         <div>
           <MyPageMenu />
-          {screen > 992 ? (
-            <RequestButtonWide
-              text="Preview your profile"
-              onClick={() => navigate('/speakers/' + id)}
-            />
-          ) : null}
         </div>
         <div className={style.outlet}>
           <Outlet />
