85bcd58fbe41123f832c468b29e0774eab77a84c
Arvind Kumar Suroju <74810455+ArvindKumarSuroju@users.noreply.github.com>
2022-07-22 19:35:09 -0700

    add: splitting booking components - booking list

---
+++ src/Containers/BookingList/BookingList.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { getSpeakerBookings } from '../../Api/Speaker.service';
+
+import tokenAtom from '../../Recoil/Authentication/atom';
+import SpeakerUpcomingList from '../../Components/SpeakerBookingList/Upcoming/SpeakerUpcomingList';
+import SpeakerHistoryList from '../../Components/SpeakerBookingList/History/SpeakerHistoryList';
+import SpeakerWaitingList from '../../Components/SpeakerBookingList/Waiting/SpeakerWaitingList';
+
+const BookingList = (props) => {
+  const [loadingState, setLoadingState] = useState(true);
+  const [speakerData, setSpeakerData] = useState();
+  const [user] = useRecoilState(tokenAtom);
+  const [newBookingData, setNewBookingData] = useState('');
+
+  useEffect(() => {
+    setLoadingState(true);
+    if (user.name._id) {
+      getSpeakerBookings(user.name._id)
+        .then((response) => {
+          // console.log(response)
+          if (response) {
+            setSpeakerData(response.data);
+            setLoadingState(false);
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [user, newBookingData]);
+  return (
+    <>
+      {loadingState ? (
+        <p>Loading... </p>
+      ) : (
+        <>
+          <SpeakerWaitingList
+            speaker={speakerData}
+            newBookingData={newBookingData}
+            setNewBookingData={setNewBookingData}
+          />
+          <SpeakerUpcomingList
+            speaker={speakerData}
+            newBookingData={newBookingData}
+            setNewBookingData={setNewBookingData}
+          />
+          <SpeakerHistoryList
+            speaker={speakerData}
+            newBookingData={newBookingData}
+            setNewBookingData={setNewBookingData}
+          />
+        </>
+      )}
+      newBookingData={setNewBookingData}
+    </>
+  );
+};
+
+export default BookingList;
