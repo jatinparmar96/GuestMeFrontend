0430105e8c98c5d3f9f48d2eac52153c73f6c1e6
Arvind Kumar Suroju <74810455+ArvindKumarSuroju@users.noreply.github.com>
2022-07-22 19:42:16 -0700

    add: speaker booking

--- src/Pages/SpeakerBooking/SpeakerBooking.jsx
+++ src/Pages/SpeakerBooking/SpeakerBooking.jsx
@@ -1,60 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { getSpeakerBookings } from '../../Api/Speaker.service';
-import SpeakerWaitingList from '../../Components/SpeakerBookingList/Waiting/SpeakerWaitingList';
+// import { useEffect, useState } from 'react';
+// import { useRecoilState } from 'recoil';
+// import { getSpeakerBookings } from '../../Api/Speaker.service';
 
-import tokenAtom from '../../Recoil/Authentication/atom';
+// import tokenAtom from '../../Recoil/Authentication/atom';
 import { Helmet } from 'react-helmet-async';
-import SpeakerUpcomingList from '../../Components/SpeakerBookingList/Upcoming/SpeakerUpcomingList';
-import SpeakerHistoryList from '../../Components/SpeakerBookingList/History/SpeakerHistoryList';
+
+import BookingList from '../../Containers/BookingList/BookingList';
 
 const SpeakerBooking = (props) => {
-  const [loadingState, setLoadingState] = useState(true);
-  const [speakerData, setSpeakerData] = useState();
-  const [user] = useRecoilState(tokenAtom);
-  const [newBookingData, setNewBookingData] = useState('');
-
-  useEffect(() => {
-    setLoadingState(true);
-    if (user.name._id) {
-      getSpeakerBookings(user.name._id)
-        .then((response) => {
-          // console.log(response)
-          if (response) {
-            setSpeakerData(response.data);
-            setLoadingState(false);
-          }
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [user, newBookingData]);
-
   return (
     <>
       <Helmet>
         <title>GUEST ME - Speaker Booking</title>
       </Helmet>
-      {loadingState ? (
-        <p>Loading... </p>
-      ) : (
-        <>
-          <SpeakerWaitingList
-            speaker={speakerData}
-            newBookingData={newBookingData}
-            setNewBookingData={setNewBookingData}
-          />
-          <SpeakerUpcomingList
-            speaker={speakerData}
-            newBookingData={newBookingData}
-            setNewBookingData={setNewBookingData}
-          />
-          <SpeakerHistoryList
-            speaker={speakerData}
-            newBookingData={newBookingData}
-            setNewBookingData={setNewBookingData}
-          />
-        </>
-      )}
+      <BookingList />
     </>
   );
 };
